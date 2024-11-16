c3c3f65ca52869e2280373c241764b354cc763b2
Jakov <jakov.gracin1@gmail.com>
2024-11-16 22:26:39 +0100

    Fix firing of text observables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,8 @@ import {
   fromEvent,
   interval,
   map,
+  merge,
+  Subject,
   switchMap,
   throttleTime,
 } from 'rxjs';
@@ -24,47 +26,43 @@ import { HashResponse } from './hash-service/hash-service.model';
 export class AppComponent {
   title = 'hash-of-text';
 
-  public listOfHashes: string[] = [];
+  public listOfHashes: HashResponse[] = [];
   public textInput: string = '';
   public textInputElement: Element | null = null;
+  private flushInputText$: Subject<string> = new Subject();
 
   constructor(private hashService: HashService) {}
 
   public enterKeyPressed(event: any) {
-    this.processCurrentTextValue(event.target.value);
+    this.flushInputText$.next(event.target.value);
   }
 
   ngOnInit() {
     this.textInputElement = document.querySelector('input.textbox');
 
     const textObservable$ = fromEvent(this.textInputElement!, 'keyup').pipe(
-      map((event: any) => event.target.value)
+      map((event: any) => event.target.value),
+      debounceTime(500),
+      throttleTime(300)
     );
 
-    textObservable$
-      .pipe(
-        debounceTime(500),
-        throttleTime(300),
+    const flushedTextObservable = merge(textObservable$, this.flushInputText$);
 
+    flushedTextObservable
+      .pipe(
         filter((inputString) => inputString.trim() !== ''),
         switchMap((inputString) => this.hashService.getHash(inputString))
       )
       .subscribe((hashResponse: HashResponse) => {
-        this.processCurrentTextValue(
-          hashResponse.input + ': ' + hashResponse.hashOfInput
-        );
+        this.processCurrentTextValue(hashResponse);
       });
 
     const deleteInterval = 4_000;
     interval(deleteInterval).subscribe(() => this.listOfHashes.shift());
-
-    // textObservable$
-    //   .pipe(debounceTime(300), throttleTime(3000))
-    //   .subscribe(() => {});
   }
 
-  public processCurrentTextValue(newTextValue: string): void {
-    this.listOfHashes.push(newTextValue);
+  public processCurrentTextValue(hashResponse: HashResponse): void {
+    this.listOfHashes.push(hashResponse);
     (this.textInputElement as HTMLInputElement).value = '';
   }
 }
