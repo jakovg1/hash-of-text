e3b1bd1b7803d547d1cd4b37f5a6aa50dfa24283
Jakov <jakov.gracin1@gmail.com>
2024-11-20 21:39:36 +0100

    Add dropdown for choosing algorithm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 // import { RouterOutlet } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import {
   catchError,
@@ -12,24 +18,28 @@ import {
   merge,
   Observable,
   of,
+  startWith,
   Subject,
   switchMap,
+  takeUntil,
+  tap,
   throttleTime,
 } from 'rxjs';
 import { HashService } from './hash-service/hash-service.service';
 import {
   HashAlgorithmResponse,
   HashOfStringResponse,
+  SupportedAlgorithmsResponse,
 } from './hash-service/hash-service.model';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule, ReactiveFormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'hash-of-text';
 
   public listOfHashes: HashOfStringResponse[] = [];
@@ -37,12 +47,24 @@ export class AppComponent {
   public textInputElement: Element | null = null;
   private flushInputText$: Subject<string> = new Subject();
 
-  public currentHashAlg$: Observable<string>;
+  private destroy$: Subject<void> = new Subject();
 
-  constructor(private hashService: HashService) {
-    this.currentHashAlg$ = this.hashService
-      .getHashAlgorithm()
-      .pipe(map((response: HashAlgorithmResponse) => response.algorithm));
+  public algorithmsForm: FormGroup = new FormGroup([]);
+  public hashAlgorithmOptions: string[] = [];
+
+  public currentHashAlg$: Observable<string>;
+  public algorithmChanged$: Subject<string> = new Subject();
+
+  constructor(private hashService: HashService, private fb: FormBuilder) {
+    this.currentHashAlg$ = this.algorithmChanged$.pipe(
+      switchMap((_) => this.hashService.getHashAlgorithm()),
+      map((response: HashAlgorithmResponse) => response.algorithm)
+    );
+  }
+
+  public ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 
   public enterKeyPressed(event: any) {
@@ -50,7 +72,45 @@ export class AppComponent {
   }
 
   ngOnInit() {
+    this.algorithmsForm = this.fb.group({
+      selectedAlgorithm: new FormControl(),
+    });
+
     this.textInputElement = document.querySelector('input.textbox');
+
+    //populate has algorithm options
+    this.hashService
+      .getSupportedHashAlgorithms()
+      .pipe(
+        map(
+          (response: SupportedAlgorithmsResponse) =>
+            response.supportedAlgorithms
+        ),
+        tap(
+          (supportedAlgorithms: string[]) =>
+            (this.hashAlgorithmOptions = supportedAlgorithms)
+        ),
+        switchMap((supportedAlgorithms: string[]) => {
+          this.algorithmChanged$.next(supportedAlgorithms[0] || '');
+          return of();
+        }),
+        takeUntil(this.destroy$),
+        catchError((err) => {
+          alert(err);
+          return of();
+        })
+      )
+      .subscribe();
+
+    //subscribe to algorithm changes
+    this.algorithmChanged$
+      .pipe(
+        switchMap((algorithm: string) =>
+          this.hashService.setHashAlgorithm(algorithm)
+        ),
+        takeUntil(this.destroy$)
+      )
+      .subscribe((foo) => console.log(foo));
 
     const textObservable$ = fromEvent(this.textInputElement!, 'keyup').pipe(
       map((event: any) => event.target.value),
@@ -66,7 +126,8 @@ export class AppComponent {
         switchMap((inputString) =>
           this.hashService.getHashOfString(inputString)
         ),
-        catchError((err) => of({} as HashOfStringResponse))
+        takeUntil(this.destroy$),
+        catchError((_) => of({} as HashOfStringResponse))
       )
       .subscribe((hashResponse: HashOfStringResponse) => {
         if (hashResponse === undefined) return;
@@ -75,6 +136,12 @@ export class AppComponent {
 
     const deleteInterval = 4_000;
     interval(deleteInterval).subscribe(() => this.listOfHashes.shift());
+  }
+
+  public onAlgorithmChange(): void {
+    const selectedAlgorithm =
+      this.algorithmsForm.get('selectedAlgorithm')?.value;
+    this.algorithmChanged$.next(selectedAlgorithm);
   }
 
   public processCurrentTextValue(hashResponse: HashOfStringResponse): void {
